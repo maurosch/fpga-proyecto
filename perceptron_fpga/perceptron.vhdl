d3783d8697b917b36a186d6e32b51f59a9a57967
Franco <franco.schiavinato95@gmail.com>
2023-06-22 17:30:48 -0300

    WIP

--- perceptron_fpga/perceptron.vhdl
+++ perceptron_fpga/perceptron.vhdl
@@ -2,19 +2,28 @@ library IEEE;
 use IEEE.std_logic_1164.all;
 use IEEE.numeric_std.all;
 
+package my_package is
+    type perceptron_input is array (natural range <>) of std_logic_vector(63 downto 0);
+end package;
+
+use work.my_package.all;
+
 entity perceptron is
+generic (
+    N: natural := 1 -- cantidad de entradas del perceptron
+);
 port (
-    peso_i : in std_logic_vector(63 downto 0);
-    input : in std_logic_vector(63 downto 0);
+    peso_i : in perceptron_input(N-1 downto 0);
+    input : in perceptron_input(N-1 downto 0);
 	clock_i : in std_logic;
 	clr_i : in std_logic;
 	enable_i : in std_logic;
-    output : out std_logic_vector(127 downto 0)
+    output : out std_logic_vector(63 downto 0)
 );
 end entity;
 
 architecture logic of perceptron is 
-    signal input_pesado : signed(127 downto 0);
+    signal input_pesado : signed(63 downto 0);
 begin
 	PERCEPTRON_PROC: process(clock_i, enable_i)
 	begin
@@ -25,7 +34,7 @@ begin
                 input_pesado <= (others => '0');
             end if;
 			if enable_i = '1' then
-                input_pesado <= signed(input) * signed(peso_i);
+                input_pesado <= signed(input(0)) * signed(peso_i(0));
                 if input_pesado(127) = '1' then
                     -- input_pesado negativo
                     output <= (others => '0');
@@ -35,4 +44,27 @@ begin
 			end if;
 		end if;
 	end process;
+end architecture;
+
+entity sum_mul is 
+port (
+    input : in std_logic_vector(63 downto 0);
+    multiplier: in std_logic_vector(63 downto 0);
+    accum: in std_logic_vector(63 downto 0);
+	clock_i : in std_logic;
+	clr_i : in std_logic;
+	enable_i : in std_logic;
+    output : out std_logic_vector(63 downto 0);
+);
+end entity;
+
+
+architecture logic of sum_mul is
+begin
+    SUM_MUL_PROC: process(clock_i, enable_i) 
+    begin
+        if rising_edge(clock_i)
+            output <= signed(input(0)) * signed(peso_i(0)) + signed(accum(0));
+        end if;
+    end process;
 end architecture;
